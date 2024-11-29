00c06ba7279abd1e98432a80babfb9b112e09717
Himanshu Mehra <himanshumehra@Himanshus-MacBook-Air.local>
2024-11-29 14:49:19 +0530

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import logo from "/logo.png";
 
 const Header = () => {
     return (
-        <header className="bg-gray-100 text-white p-5 flex justify-center items-center">
-            <img src={logo} alt="Trello logo" className="w-40" />
+        <header className="bg-blue-500 text-white p-5 flex justify-center items-center">
+            <h1 className="text-5xl font-bold">Trello</h1>
         </header>
     );
 };
